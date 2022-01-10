ee21f059c2e22a9fae56a020ae3605db94f8af0f
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-01-10 18:16:44 -0300

    [feat]:requisito 2 - adicionou a estrutura basica do componente Header e adicionado a pagina Carteira

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    // const { email } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">
+          email
+        </span>
+        <span data-testid="total-field">
+          { 0 }
+        </span>
+        <span data-testid="header-currency-field">
+          BRL
+        </span>
+      </header>
+    );
+  }
+}
+
+// Header.propTypes = {
+//   email: PropTypes.string.isRequired,
+// };
+
+export default Header;
