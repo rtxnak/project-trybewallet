889c0037b656e4808ee86e6a76f2fe9941dbec05
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-01-11 15:15:29 -0300

    [feat]:requisito 6 - adicionou estrutura de tabela de gastos basica

---
+++ src/components/ExpenseTable.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class ExpenseTable extends React.Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Descrição</td>
+            <td>Tag</td>
+            <td>Método de pagamento</td>
+            <td>Valor</td>
+            <td>Moeda</td>
+            <td>Câmbio utilizado</td>
+            <td>Valor convertido</td>
+            <td>Moeda de conversão</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default ExpenseTable;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import ExpenseForm from '../components/ExpenseForm';
+import ExpenseTable from '../components/ExpenseTable';
 
 class Wallet extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class Wallet extends React.Component {
         <Header />
         <p> TrybeWallet </p>
         <ExpenseForm />
+        <ExpenseTable />
       </div>
     );
   }
